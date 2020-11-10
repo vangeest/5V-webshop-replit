0671c1e38243aa59e11c1598489bfe452ba06de1
Notalifeform <notalifeform@gmail.com>
2020-11-10 20:06:04 +0000

    centralised basket logic

--- public/js/basket.js
+++ public/js/basket.js
@@ -32,11 +32,47 @@ function addToBasket(id, amount, showEl) {
 
 function removeFromBasket(id, amount, showEl) {
   var basket = readBasket()
-  basket[id] =  Math.max(0, ((basket[id]||0) - amount))
+  basket[id] =  Math.max(1, ((basket[id]||0) - amount))
   localStorage.setItem('basket', JSON.stringify(basket))
   if(showEl) {
     showEl.textContent = basket[id]
   }
 }
 
+function updateBasket(value){
+  const basket = value||readBasket()
+  var num = 0;
+  for (let key in basket) {
+    num += parseInt(basket[key])
+  }
+  if( num == 0){
+    window.basket_num.textContent = "(leeg)"
+  } else if( num == 1) {
+    window.basket_num.textContent = `(${num} item)`
+  } else {
+    window.basket_num.textContent = `(${num} items)`
+  }
+}
+
+(function() {
+  // your page initialization code here
+  // the DOM will be available here
+  window.basket_num = document.querySelector('.basket_num')
+  var originalSetItem = localStorage.setItem;
+  localStorage.setItem = function(key, value) {
+    var event = new Event('itemInserted');
+    event.value = value; // Optional..
+    event.key = key; // Optional..
+    document.dispatchEvent(event);
+    originalSetItem.apply(this, arguments);
+  };
+
+  var localStorageSetHandler = function(e) {
+    if(e.key === "basket") {
+      updateBasket(JSON.parse(e.value))
+    }
+  };
+  document.addEventListener("itemInserted", localStorageSetHandler, false);
+
+})();
 
