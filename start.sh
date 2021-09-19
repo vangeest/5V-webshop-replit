614a95c31a4671e977f8d21d4c991afe435bee5b
vangeest <gee@emmauscollege.nl>
2021-09-19 19:22:42 +0000

    updates start.dh

--- start.sh
+++ start.sh
@@ -1,6 +1,12 @@
 echo installing node.js packages according to package-lock.json file
+echo this may take some minutes the first time
 npm install
-echo genenating database from db.sql and saving to db.sqlite3
-sqlite3 main.db < main.sql
+
+echo genenating database from create.sql and saving to my.db
+cd db
+sqlite3 my.db < create.sql
+cd ..
+
 echo starting api and static webserver
+cd api
 node index.js
